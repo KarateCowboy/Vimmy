0c505bc986659cea8caf5df85fbc51cbeb3739c5
karatecowboy <moriger@mykolab.com>
2021-03-05 23:15:35 -0600

    misc cards

--- cards.js
+++ cards.js
@@ -1,16 +1,16 @@
 const allMyCards = {
-  rescript: [
-    [ "Rescript multi-line comment", "slash and star" ],
-    [ "Rescript value assignment", "let x = 5"],
-    [ "Rescript string delimiter", `must be " double quotes`],
-    [ "Rescript string concatenation operator", "++" ],
-    [ "Rescript template strings", "Use backticks for delimiters and ${} for interpolation"],
-    [ "Rescript booleans", " true and false"],
+  "rescript": [
+    [ "multi-line comment", "slash and star" ],
+    [ "value assignment", "let x = 5"],
+    [ "string delimiter", `must be " double quotes`],
+    [ "string concatenation operator", "++" ],
+    [ "template strings", "Use backticks for delimiters and ${} for interpolation"],
+    [ "booleans", " true and false"],
     [ "Resscript negation operator", "!"],
-    [ "Rescript modulus operator", " mod(num, den)"],
-    [ "Rescript floating point addition operator", "+."],
-    [ "Rescript record or object declaration", "type point = { x: int, y: int }"],
-    [ "Rescript object destructuring operator", `...someObj followed by value overrides`],
+    [ "modulus operator", " mod(num, den)"],
+    [ "floating point addition operator", "+."],
+    [ "record or object declaration", "type point = { x: int, y: int }"],
+    [ "object destructuring operator", `...someObj followed by value overrides`],
     [ "Are mixed type arrays allowed in Rescript?", "No. Only a tuple may do that."],
     [ "What is the closest Rescript type to a null value", "the None type"],
     [ "Are Rescript if statements expressions?", "yes, they are" ]
@@ -19,5 +19,14 @@ const allMyCards = {
     [ "The four building blocks of Nix", "Nix store, derivations, sandboxing, and the Nix language" ],
     ["The central location of all Nix packages in the system", "/nix/store" ],
     ["The type of storage model closest to what the Nix Store uses", "A Graph Database" ]
+  ],
+  "loopback4": [
+    ["set up a new data source", "lb4 datasource"],
+    ["create a new model", "lb4 model"],
+    [""]
+  ],
+  "mongodb": [
+    ["the default port on which the server runs", "27017"],
+    ["exit the shell", "quit()"]
   ]
 }
